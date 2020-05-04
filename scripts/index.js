96ac640a979bb55ab054dd04b8e22cbd714ee8e3
Sami Hattab <hattab_sami@yahoo.fr>
2020-05-04 15:41:35 +0200

    the songs now display

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,37 @@
             url: `https://api.deezer.com/search?q=${titre}&output=jsonp&order=${sort}`,
             dataType: "jsonp",
         }).then((result) => {
-            console.log("Résultat :", result.data);
-        })
 
-    }
+            const results = result.data;
+            console.log(results)
+            let content = '';
+
+            $.each(results, function (index, result) {
+                content +=
+                    `<div class='track'>
+                    <div>
+                        <img src='${result.album.cover}' alt="">
+                        <div class='track-data'>
+                        <h1>${result.title_short}</h1>
+                        <p>${result.artist.name}</p>
+                        </div>
+                    </div>
+                <audio controls src="${result.preview}"></audio>
+                <button id='${result.id}' class='fav' onclick='${addFav(this)}'>Ajouter aux favoris</button>
+            </div>`
+
+            });
+            $('#tracks').html(content);
+
+            // $('.fav').on('click', function(e) {
+            //     e.preventDefault();
+            //     addFav($(this).attr(this.id));
+            // });
+
+            function addFav(id) {
+                console.log(id);
+            }
+        });
+
+    };
 })();
