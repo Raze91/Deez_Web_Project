ec366b068448e54096ed5c27d6da8cfcceb602d8
Sami Hattab <hattab_sami@yahoo.fr>
2020-05-05 10:55:47 +0200

    user can now save a song as a favorite, the favorite songs are displayed in the third page

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,34 @@
 (function () {
     'use strict';
 
+    const favStorage = localStorage;
+    let favTracks = [];
+
     $('#submit').on('click', onSubmit);
+
+    if (favStorage.getItem('favoris')) {
+        favTracks = JSON.parse(favStorage.getItem('favoris'));
+
+        $.each(favTracks, function (index, fav) {
+            $('#favTracks').append(
+                `<div class='track'>
+                            <div>
+                                <img src='${fav.album.cover}' alt="">
+                                <div class='track-data'>
+                                <h1>${fav.title_short}</h1>
+                                <p>${fav.artist.name}</p>
+                                <button id='${fav.id}' class='fav'>Ajouter aux favoris</button>
+                                </div>
+                            </div>
+                            <audio controls src="${fav.preview}"></audio>
+                        </div>`
+            )
+        })
+    };
 
     function onSubmit(event) {
         event.preventDefault();
+        $('#tracks').empty();
 
         let titre = $('#title').val();
         let sort = $('#sortBy').val();
@@ -16,34 +40,46 @@
 
             const results = result.data;
             console.log(results)
-            let content = '';
 
             $.each(results, function (index, result) {
-                content +=
+                $('#tracks').append(
                     `<div class='track'>
-                    <div>
-                        <img src='${result.album.cover}' alt="">
-                        <div class='track-data'>
-                        <h1>${result.title_short}</h1>
-                        <p>${result.artist.name}</p>
+                        <div>
+                            <img src='${result.album.cover}' alt="">
+                            <div class='track-data'>
+                            <h1>${result.title_short}</h1>
+                            <p>${result.artist.name}</p>
+                            <button id='${result.id}' class='fav'>Ajouter aux favoris</button>
+                            </div>
                         </div>
-                    </div>
-                <audio controls src="${result.preview}"></audio>
-                <button id='${result.id}' class='fav' onclick='${addFav(this)}'>Ajouter aux favoris</button>
-            </div>`
+                        <audio controls src="${result.preview}"></audio>
+                    </div>`
+                );
 
-            });
-            $('#tracks').html(content);
+                $(`#${result.id}`).on('click', function (e) {
+                    e.preventDefault();
+                    addFav(result);
+                });
 
-            // $('.fav').on('click', function(e) {
-            //     e.preventDefault();
-            //     addFav($(this).attr(this.id));
-            // });
+            })
 
-            function addFav(id) {
-                console.log(id);
+
+            function addFav(result) {
+                // console.log(result);
+                favTracks.push(result);
+                favStorage.setItem('favoris', JSON.stringify(favTracks));
+
+
             }
-        });
+        })
+            .catch((err) => {
+                if (err) throw err;
+            })
 
-    };
+    }
+
+
+
+
+
 })();
